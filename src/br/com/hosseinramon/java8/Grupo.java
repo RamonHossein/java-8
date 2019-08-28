5181fbc51a978cdddbb02f79a9f62480c5f674da
Ramon Hossein <hosseinramon@gmail.com>
2019-08-27 22:28:16 -0300

    Capitulo 8

---
+++ src/br/com/hosseinramon/java8/Grupo.java
@@ -0,0 +1,18 @@
+package br.com.hosseinramon.java8;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Grupo {
+
+	private Set<Usuario> usuarios = new HashSet<>();
+	
+	public void add(Usuario u) {
+		usuarios.add(u);
+	}
+	
+	public Set<Usuario> getUsuario() {
+		return Collections.unmodifiableSet(this.usuarios);
+	}
+}
